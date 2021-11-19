8fdb9dc7f2822a42be6176b90eb52060f3fff32d
thsmcoding <hassia.tamimou@gmail.com>
2021-11-19 18:19:31 +0100

    API: added further use-package instructions

--- Virt-C++/cstarter.el
+++ Virt-C++/cstarter.el
@@ -27,22 +27,32 @@
 (use-package which-key :ensure t :config (which-key-mode))
 	     
 ;;AUTO-COMPLETE
-(require 'auto-complete-config)
-(ac-config-default)
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)
+))
+
 
 ;;GETTING YASNIPPET
 (add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
-(require 'yasnippet)
-(yas-global-mode 1)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+(use-package yasnippet
+  :ensure t
+  :init
+  (progn
+    (yas-global-mode 1)
+    (custom-set-variables
+     ;; custom-set-variables was added by Custom.
+     ;; If you edit it by hand, you could mess it up, so be careful.
+     ;; Your init file should contain only one such instance.
+     ;; If there is more than one, they won't work right.
  '(package-selected-packages '(yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ )    
+ ))
